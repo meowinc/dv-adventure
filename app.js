764a5595480c51c7d56d8da663a549c6607f20cc
meow <imajni@gmail.com>
2020-09-14 01:10:31 -0400

    update cache

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ var app = new Vue({
   },
   created: function() {
     let $this = this;
-    Papa.parse("values.csv", {
+    Papa.parse("values.csv?v=0002", {
       download: true,
       header: true,
       step: function(row) {
