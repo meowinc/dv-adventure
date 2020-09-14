25ad560108111495cc8be67688baeeb9b61edd9e
meow <imajni@gmail.com>
2020-09-14 01:20:56 -0400

    update version

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ var app = new Vue({
   },
   created: function() {
     let $this = this;
-    Papa.parse("values.csv?v=0002", {
+    Papa.parse("values.csv?v=0003", {
       download: true,
       header: true,
       step: function(row) {
