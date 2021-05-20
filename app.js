bb03ce76e180cf550e1e98252038f38c4feb9d28
meowinc <imajni@gmail.com>
2021-05-19 23:24:13 -0700

    add new riddles

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ var app = new Vue({
   },
   created: function() {
     let $this = this;
-    Papa.parse("values.csv?v=0007", {
+    Papa.parse("values.csv?v=0008", {
       download: true,
       header: true,
       step: function(row) {
