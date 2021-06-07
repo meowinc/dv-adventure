72e52c99730752022a26257f42807745062fcc76
meowinc <imajni@gmail.com>
2021-06-06 21:56:33 -0700

    Sync

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ var app = new Vue({
   },
   created: function() {
     let $this = this;
-    Papa.parse("values.csv?v=0014", {
+    Papa.parse("values.csv?v=0015", {
       download: true,
       header: true,
       step: function(row) {
